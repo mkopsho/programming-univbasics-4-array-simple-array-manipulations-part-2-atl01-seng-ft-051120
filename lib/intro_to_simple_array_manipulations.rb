9e2928df9120f4837ca52bb1a3b1556840b126d1
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-08 21:21:24 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,4 +2,6 @@ def using_concat(my_favorite_things, more_favs)
   all_my_favs = my_favorite_things.concat(more_favs)
 end
 
-def using_insert()
+def using_insert(list_of_programming_languages, another_language)
+  
+end
