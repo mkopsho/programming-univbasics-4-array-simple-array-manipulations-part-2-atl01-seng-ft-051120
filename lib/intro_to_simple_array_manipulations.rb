40a2c23eb075f2bf0b5b0116bd5b6332947bd692
Michael Kopsho <michael.kopsho@gmail.com>
2020-04-08 21:25:00 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -6,6 +6,6 @@ def using_insert(list_of_programming_languages, another_language)
   list_of_programming_languages.insert(4, another_language)
 end
 
-def using uniq(array)
+def using_uniq(array)
   new_array = array.uniq
 end
